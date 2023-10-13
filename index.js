65c6a1c5a784c4f6975e1c730662a2c099aaaf6a
Will-kor <85548787+Will-kor@users.noreply.github.com>
2023-10-14 00:55:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -413,11 +413,10 @@
   }
 
   function newBall(x, y, size) {
-<<<<<<< HEAD
     if (size === 1) size = 2;  // Adjust the first size to the second size
     
     let polyBody;
-=======
+
     c = Bodies.circle(x, y, size * 10 * 1.5, {  // 1.8배로 늘림
       render: {
         sprite: {
@@ -431,7 +430,6 @@
     c.createdAt = Date.now();
     c.restitution = 0.2;
     c.friction = 0.6;
->>>>>>> 413519774c45186b14dc16d153195e54ee1b1088
 
     const radius = size*11; // Adjust this value for the desired size of the nonagon
       polyBody = Bodies.polygon(x, y, 9, radius, {
