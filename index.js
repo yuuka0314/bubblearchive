235ff912704660200dc1aaeba79a9d93b9025b4d
Will-kor <85548787+Will-kor@users.noreply.github.com>
2023-10-13 09:54:59 +0900

    Update index.js
    안고쳐져서 복구

--- index.js
+++ index.js
@@ -93,10 +93,6 @@
   addEventListener("touchend", () => {
     if (isGameOver) return;
 
-    const ballRadius = ball.size * 10 * 1.5;
-    if (ball.position.x > render.options.width - ballRadius) ball.position.x = render.options.width - ballRadius;
-    else if (ball.position.x < ballRadius) ball.position.x = ballRadius;
-
     isClicking = false;
 
     if (isGameOver) return;
