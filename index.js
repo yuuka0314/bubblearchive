37a003157e73b958f57625701d385fcbea606c74
Will-kor <85548787+Will-kor@users.noreply.github.com>
2023-10-13 11:10:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -119,7 +119,7 @@
     const rect = canvas.getBoundingClientRect();
     mousePos = e.clientX / parent.style.zoom - rect.left;
   });
-  const MAX_SPEED = 50; // 공이 한 프레임에서 움직일 수 있는 최대 속도
+  const MAX_SPEED = 2000; // 공이 한 프레임에서 움직일 수 있는 최대 속도
   addEventListener("touchmove", (e) => {
     if (isGameOver) return;
 
