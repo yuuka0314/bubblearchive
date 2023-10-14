e95b7434d87f3ba59f507908b0e90eeba1a8df02
Will-kor <0koreawill0@gmail.com>
2023-10-14 17:30:43 +0900

    .

--- index.js
+++ index.js
@@ -416,23 +416,6 @@
     if (size === 1) size = 2;  // Adjust the first size to the second size
     
     let polyBody;
-<<<<<<< HEAD
-=======
-
-    c = Bodies.circle(x, y, size * 10 * 1.5, {  // 1.8배로 늘림
-      render: {
-        sprite: {
-          texture: `assets/img/${size}.png`,
-          xScale: size / 12.75 * 1.5,  // 1.8배로 늘림
-          yScale: size / 12.75 * 1.5,  // 1.8배로 늘림
-        },
-      },
-    });
-    c.size = size;
-    c.createdAt = Date.now();
-    c.restitution = 0.2;
-    c.friction = 0.6;
->>>>>>> 65c6a1c5a784c4f6975e1c730662a2c099aaaf6a
 
     const radius = size*11; // Adjust this value for the desired size of the nonagon
       polyBody = Bodies.polygon(x, y, 9, radius, {
